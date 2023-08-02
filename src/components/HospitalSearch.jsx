6aa415bc4b6a89a1a6d1381a752dc44b55ac2265
sani4618 <sanijoseph2018@gmail.com>
2023-08-02 12:31:16 +0530

    navigation bar creation successfully completed

--- src/components/HospitalSearch.jsx
+++ src/components/HospitalSearch.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavigationBar from './NavigationBar'
 
 const HospitalSearch = () => {
   return (
     <div>
+        <NavigationBar/>
 
         <div className="container">
             <div className="row">
